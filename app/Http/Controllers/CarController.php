8157f97938b51df8a83f9e05c0294a1a5f2434d3
NikolaJ <jovanovicn369@gmail.com>
2021-11-23 19:43:24 +0100

    2nd task

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::all();
+
+        return response()->json($cars);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $car = Car::create([
+            'brand' => $request->get('brand',),
+            'model' => $request->get('model',),
+            'year' => $request->get('year',),
+            'max_speed' => $request->get('max_speed',),
+            'is_automatic' => $request->get('is_automatic',),
+            'engine' => $request->get('engine',),
+            'number_of_doors' => $request->get('number_of_doors',),
+        ]);
+
+        return response()->json($car);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $car = Car::findOrFail($id);
+
+        return response()->json($car);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $car = Car::find($id);
+        $car->update($request->all());
+
+        return response()->json($car);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $car = Car::find($id);
+        $car->delete();
+
+        return response()->json($car);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CarController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/cars', [CarController::class, 'index']);
+Route::get('/cars/{id}', [CarController::class, 'show']);
+Route::post('cars', [CarController::class, 'store']);
+Route::put('cars/{id}', [CarController::class, 'update']);
+Route::delete('cars/{id}', [CarController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
